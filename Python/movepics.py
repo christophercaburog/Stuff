77e9aac0458233e193d4c815497d1b9dbc17e73d
Klong <klong@klong.nu>
2015-05-21 02:23:08 +0200

    Fixing indentation of Python files

--- Python/movepics.py
+++ Python/movepics.py
@@ -1,17 +1,17 @@
 import os
 
-dir = os.getcwd()+"/";
-todir = dir+"Bilder/"
+dir = os.getcwd() + "/"
+todir = dir + "Bilder/"
 try:
-	os.mkdir(todir)
+    os.mkdir(todir)
 except OSError:
-	print "Directory exists"
+    print "Directory exists"
 print dir, todir
 for file in os.listdir(dir):
-	filel=file.lower()
-	if filel.endswith(".jpg") or filel.endswith(".png") or filel.endswith(".gif"):
-		try:
-			print dir+file
-			os.rename(dir+file,todir+file)
-		except OSError:
-			print "Error: "+file
+    filel = file.lower()
+    if filel.endswith(".jpg") or filel.endswith(".png") or filel.endswith(".gif"):
+        try:
+            print dir + file
+            os.rename(dir + file, todir + file)
+        except OSError:
+            print "Error: " + file
